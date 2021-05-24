b83423d889ca89c0d28970370fa99e7722aa8586
Rohit Awate <rohitawate121@gmail.com>
2021-05-24 23:11:55 +0530

    Implement DeduceTypeForColumn

---
+++ commaql/engine/transform.go
@@ -0,0 +1,31 @@
+package engine
+
+import "strconv"
+
+type SQLType int
+
+const (
+	SQL_INT    = iota
+	SQL_FLOAT  = iota
+	SQL_STRING = iota
+	SQL_BOOL   = iota
+)
+
+func DeduceTypeForColumn(sample string) SQLType {
+	_, err := strconv.Atoi(sample)
+	if err == nil {
+		return SQL_INT
+	}
+
+	_, err = strconv.ParseFloat(sample, 64)
+	if err == nil {
+		return SQL_FLOAT
+	}
+
+	_, err = strconv.ParseBool(sample, 64)
+	if err == nil {
+		return SQL_FLOAT
+	}
+
+	return SQL_STRING
+}
